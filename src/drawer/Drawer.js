27ac2931b33b94056253061a60c56fbf8dc73f18
Petr Krejčík <pkrejcik@soincon.es>
2020-04-20 16:00:21 +0200

    drawer

---
+++ src/drawer/Drawer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { List, ListItem, ListItemText, Divider, SwipeableDrawer } from '@material-ui/core';
+import { useLogin, useLogout } from 'auth/Auth';
+import { globalStateContext } from 'app/GlobalStateContext';
+import { Dialog } from 'dialog/Dialog';
+
+const useStyles = makeStyles({
+  root: {
+    width: 200,
+  },
+});
+
+export const Drawer = (props) => {
+  const classes = useStyles();
+  const logout = useLogout();
+  const [loginState, login] = useLogin();
+  const { userState } = React.useContext(globalStateContext);
+  const [user] = userState;
+  const [isErrorOpen, setErrorOpen] = React.useState(true);
+
+  const handleCloseError = () => setErrorOpen(false);
+
+  return (
+    <SwipeableDrawer
+      open={props.open}
+      onClose={props.toggle}
+      onOpen={props.toggle}
+      className={classes.root}
+    >
+      <List>
+        {loginState.error && (
+          <Dialog handleClose={handleCloseError} open={isErrorOpen} title="Login error">
+            {loginState.error.message}
+          </Dialog>
+        )}
+        <ListItem button>
+          {user ? (
+            <ListItemText primary="Logout" onClick={logout} />
+          ) : (
+            <ListItemText primary="Login" onClick={login} />
+          )}
+        </ListItem>
+        <Divider />
+      </List>
+    </SwipeableDrawer>
+  );
+};
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Button } from '@material-ui/core';
+import { AppBar } from '@material-ui/core';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import { useLogin, useLogout } from 'auth/Auth';
-import { globalStateContext } from 'app/GlobalStateContext';
-import { Dialog } from 'dialog/Dialog';
+import { Drawer } from 'drawer/Drawer';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,73 +20,33 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const Header = () => {
-  const logout = useLogout();
-  const [loginState, login] = useLogin();
-  const { userState } = React.useContext(globalStateContext);
-  const [user] = userState;
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [isErrorOpen, setErrorOpen] = React.useState(true);
-  const open = Boolean(anchorEl);
+  const [drawerOpened, openDrawer] = React.useState(false);
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
+  const toggleDrawer = (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    openDrawer(!drawerOpened);
   };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleCloseError = () => setErrorOpen(false);
 
   return (
     <div className={classes.root}>
-      {loginState.error && (
-        <Dialog handleClose={handleCloseError} open={isErrorOpen} title="Login error">
-          {loginState.error.message}
-        </Dialog>
-      )}
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            onClick={toggleDrawer}
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}></Typography>
-          {user ? (
-            <div>
-              <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={open}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={logout}>Logout</MenuItem>
-              </Menu>
-            </div>
-          ) : (
-            <Button onClick={login}>Login</Button>
-            // <Login />
-          )}
         </Toolbar>
       </AppBar>
+      <Drawer open={drawerOpened} toggle={toggleDrawer} />
     </div>
   );
 };
