e85f12620b684c7b5dbbc5a022853dafe1cb9025
Petr Krejčík <pkrejcik@soincon.es>
2020-04-22 16:31:52 +0200

    add new

--- src/product/AddIngredient.js
+++ src/product/AddIngredient.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button, TextField, IconButton, Grid } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
-import { Add } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+import { Add, CheckBoxOutlineBlank } from '@material-ui/icons';
 import { updateIngredient, addIngredient, validateIngredient } from './ingredientUtils';
 import { globalStateContext } from 'app/GlobalStateContext';
 
 export const AddIngredient = (props) => {
+  const classes = useStyles();
   const [newIngredient, setNewIngredient] = React.useState('');
   const [isTypingNew, setIsTypingNew] = React.useState(false);
   const { userState, inputState } = React.useContext(globalStateContext);
@@ -39,47 +41,73 @@ export const AddIngredient = (props) => {
     setNewIngredient(newIngredient);
   };
 
+  const handleBlur = () => {
+    if (newIngredient) return;
+    clearInput();
+  };
+
   return (
     <>
       <Grid container alignItems="center">
-        <Grid item>
-          <IconButton>
-            <Add />
-          </IconButton>
-        </Grid>
-        <Grid item xs={10}>
-          {isTypingNew ? (
-            <Autocomplete
-              options={props.ingredients.filter(({ available }) => available)}
-              getOptionLabel={(option) => option.title || option}
-              freeSolo
-              autoFocus
-              inputValue={newIngredient}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Add"
-                  autoFocus
-                  onFocus={() => focusInput(true)}
-                  onBlur={() => focusInput(false)}
-                />
-              )}
-              onInputChange={handleType}
-              onChange={handleAutocompleteSelect}
-              onClose={(event, reason) => {
-                if (reason === 'select-option') return;
-                handleConfirmNew();
-              }}
-            />
-          ) : (
-            <>
-              <Button onClick={() => setIsTypingNew(true)} color="primary">
-                Add ingredient
-              </Button>
-            </>
-          )}
-        </Grid>
+        {isTypingNew ? (
+          <>
+            <Grid item>
+              <IconButton>
+                <CheckBoxOutlineBlank />
+              </IconButton>
+            </Grid>
+            <Grid item xs={9}>
+              <Autocomplete
+                options={props.ingredients.filter(({ available }) => available)}
+                getOptionLabel={(option) => option.title || option}
+                freeSolo
+                autoFocus
+                inputValue={newIngredient}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Add"
+                    autoFocus
+                    onFocus={() => focusInput(true)}
+                    onBlur={handleBlur}
+                  />
+                )}
+                onInputChange={handleType}
+                onChange={handleAutocompleteSelect}
+                onClose={(event, reason) => {
+                  if (reason === 'select-option') return;
+                  handleConfirmNew();
+                }}
+                classes={classes}
+              />
+            </Grid>
+            {/* {newIngredient && <AddRow onClick={() => setIsTypingNew(true)} />} */}
+          </>
+        ) : (
+          <AddRow onClick={() => setIsTypingNew(true)} />
+        )}
       </Grid>
     </>
   );
 };
+
+const AddRow = (props) => {
+  return (
+    <>
+      <Grid item>
+        <IconButton>
+          <Add />
+        </IconButton>
+      </Grid>
+      <Button onClick={props.onClick} color="primary">
+        Add ingredient
+      </Button>
+    </>
+  );
+};
+
+const useStyles = makeStyles({
+  clearIndicator: {
+    visibility: 'visible',
+  },
+});
