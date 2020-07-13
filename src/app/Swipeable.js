b50f46367d7c7f8ec5672fea3c15fd837c7e5347
Petr Krejčík <pkrejcik@soincon.es>
2020-07-14 00:49:23 +0200

    scroll to top after change screen

--- src/app/Swipeable.js
+++ src/app/Swipeable.js
@@ -2,8 +2,20 @@ import React from 'react';
 import SwipeableViews from 'react-swipeable-views';
 
 export const Swipeable = (props) => {
+  const containerRef = React.useRef();
+
+  const onTransitionEnd = () => {
+    containerRef.current.containerNode.parentNode.scrollTop = 0;
+  };
+
   return (
-    <SwipeableViews {...props} disabled style={{ height: '100%' }}>
+    <SwipeableViews
+      {...props}
+      disabled
+      style={{ height: '100%' }}
+      ref={containerRef}
+      onTransitionEnd={onTransitionEnd}
+    >
       {props.children}
     </SwipeableViews>
   );
